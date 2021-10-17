6a43a4fcdf957cd97e8714f3ddd0b7e6a1396534
oliabhi <oliabhi26@gmail.com>
2021-10-17 10:44:02 +0530

    cleaning up

--- data/createTable.sql
+++ data/createTable.sql
@@ -1,5 +1,7 @@
 --CREATING PATIENTS TABLE
-
+-- db name - IncubyteHospital
+-- table name - PATIENTS
+-- holds data for every patient that has been vaccinated
 Create TABLE PATIENTS
 (
     CUSTOMER_NAME VARCHAR(255) NOT NULL,
@@ -13,11 +15,11 @@ Create TABLE PATIENTS
     DOB DATE,
     FLAG CHAR(1)
 );
-
+--adding dummy data for test cases
 INSERT INTO IncubyteHospital.PATIENTS 
 VALUES(
     'ABHI','1','2021-10-10','2021-10-10','CVS','HARI','UP','IND','1997-05-22','N'),
     'NEHA','2','2021-10-9','2021-10-9','CVS','HARI','SYDNEY','AUS','1996-04-01','N'),
     'JAMES','3','2021-10-1','2021-10-1','CVS','HARI','SYDNEY','AUS','1991-08-01','N'),
-    'NAVIN','4','2021-09-1','2021-09-1','CVS','HARI','MP','IND','1967-08-01','N')
-);
+    'NAVIN','4','2021-09-1','2021-09-1','CVS','HARI','MP','IND','1967-08-01','N'
+    );
