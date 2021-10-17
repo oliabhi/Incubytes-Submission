56bc20e254c9e10cef1c709b0e2b13af73c620c4
oliabhi <oliabhi26@gmail.com>
2021-10-17 10:11:44 +0530

    logic for fetching data via country added

--- connector.py
+++ connector.py
@@ -28,5 +28,15 @@ Fetching rows from tables
 table_rows = cursor.fetchall()
 df = pd.read_sql('SELECT * FROM patients', con=database)  # fitting into pandas dataframe
 df.set_index(['cust_ID'], inplace=True)  # setting default index
-# print (df)
 ans = df.loc[df['Country'] == "IND"]
+
+"""
+Function show_data :
+parameter : country 
+return : country specific data present in table
+"""
+
+
+def show_data(country):
+    data = df.loc[df['Country'] == country]
+    print(data)
